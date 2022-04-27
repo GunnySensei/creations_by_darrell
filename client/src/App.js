4793d1b9238ce6d2a934ffb81871ec13222ab6a2
GunnySensei <gunnerwagoner@hotmail.com>
2022-04-26 20:16:10 -0500

    app setup

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Creations By Darrell</header>
+    </div>
+  );
+}
+
+export default App;
